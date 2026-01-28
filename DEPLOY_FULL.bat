5700030c433187daff0020f8acc4be745abea33b
Albert <albert@treino.com>
2026-01-28 10:14:46 -0300

    feat: sistema de treino consciente + hacks naturais

---
+++ DEPLOY_FULL.bat
@@ -0,0 +1,32 @@
+@echo off
+echo ========================================
+echo    DEPLOY COMPLETO (COMMIT + PUSH)
+echo ========================================
+echo.
+
+cd /d "%~dp0"
+set "GIT=C:\Program Files\Git\cmd\git.exe"
+
+echo [1/5] Adicionando arquivos...
+"%GIT%" add .
+
+echo.
+echo [2/5] Commitando alteracoes...
+"%GIT%" commit -m "feat: Add holistic dashboard reporting"
+
+echo.
+echo [3/5] Enviando Master...
+"%GIT%" push origin master
+
+echo.
+echo [4/5] Atualizando GH-PAGES...
+"%GIT%" checkout gh-pages
+"%GIT%" merge master -m "Merge master into gh-pages"
+"%GIT%" push origin gh-pages
+
+echo.
+echo [5/5] Finalizando...
+"%GIT%" checkout master
+
+echo.
+echo [SUCESSO] Site atualizado!
