233c7ec0566ca62007f7d4392ddb6c8e01ea09c6
Albert <albert@treino.com>
2026-01-28 10:59:40 -0300

    feat: Add holistic dashboard reporting

---
+++ DEPLOY_FULL.bat
@@ -0,0 +1,40 @@
+@echo off
+echo ========================================
+echo    DEPLOY COMPLETO (COMMIT + PUSH)
+echo ========================================
+echo.
+
+cd /d "%~dp0"
+set "GIT=C:\Program Files\Git\cmd\git.exe"
+
+if not exist "%GIT%" (
+    echo [ERRO] Git nao encontrado em: %GIT%
+    echo Verifique a instalacao do Git.
+    pause
+    exit
+)
+
+echo [1/5] Adicionando arquivos...
+"%GIT%" add .
+
+echo.
+echo [2/5] Commitando alteracoes...
+"%GIT%" commit -m "feat: Add holistic dashboard reporting"
+
+echo.
+echo [3/5] Enviando Master...
+"%GIT%" push origin master
+
+echo.
+echo [4/5] Atualizando GH-PAGES...
+"%GIT%" checkout gh-pages
+"%GIT%" merge master -m "Merge master into gh-pages"
+"%GIT%" push origin gh-pages
+
+echo.
+echo [5/5] Finalizando...
+"%GIT%" checkout master
+
+echo.
+echo [SUCESSO] Site atualizado!
+pause
